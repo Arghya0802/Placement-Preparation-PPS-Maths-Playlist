9edf6b48d1e06022d5f83efed5b869b8cc9d6d2d
Arghya Chatterjee <96080808+Arghya0802@users.noreply.github.com>
2022-11-14 22:06:15 +0530

    Create Check Prime.cpp

---
+++ Check Prime.cpp
@@ -0,0 +1,16 @@
+int isPrime(int N)
+{
+        if(N == 1) // For 1, we directly return false
+            return 0 ;
+            
+        for(int i = 2 ; i * i <= N ; i++) // We go till Sqrt(N) to check if it's Prime or Not
+        {
+            if(N % i == 0)
+                return 0 ;
+        }
+        
+        return true ;
+ }
+
+// Time Complexity: O(Sqrt(N))
+// Space Complexity: O(1)
